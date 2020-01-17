8bce0dcddf14c5581c8c73664a875b032ffeec5e
Evgenia Petukhova <chet444@mail.ru>
2020-01-18 00:53:54 +0500

    add switching the dots in js

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 let newsHeader = [...document.getElementsByClassName('news__item-header')];
 let newsPhoto = [...document.getElementsByClassName('news__item-photo')];
 let newsText = [...document.getElementsByClassName('news__item-text')];
-
+let circles = [...document.getElementsByClassName('nav__circle-item')];
+console.log(circles);
 const wrapHeader = document.getElementById('nav__news-header');
 const wrapPhoto = document.getElementById('nav__news-photo');
 const wrapText = document.getElementById('nav__news-text');
 
+wrapPhoto.setAttribute('src', newsPhoto[0].getAttribute('src'));
+wrapHeader.innerHTML = newsHeader[0].innerHTML;
+wrapText.innerHTML = newsText[0].innerHTML;
+circles[0].classList.add('nav__circle-item-active');
 
 
-let circles = [...document.getElementsByClassName('nav__circle-item')];
 
 let i = 0;
 let newsInterval = setInterval(nextNews, 6000);
@@ -17,6 +21,11 @@ function nextNews() {
 	wrapPhoto.setAttribute('src', newsPhoto[i].getAttribute('src'));
  	wrapHeader.innerHTML = newsHeader[i].innerHTML;
  	wrapText.innerHTML = newsText[i].innerHTML;
+ 	if(circles[i-1]) {circles[i-1].classList.remove("nav__circle-item-active");};
+ 	if( circles[5].classList.length == 2 && i==0) {
+ 		circles[5].classList.remove("nav__circle-item-active");
+ 	 	};
+ 	circles[i].classList.add('nav__circle-item-active');
 	i=(i+1)%6;
 }
 
