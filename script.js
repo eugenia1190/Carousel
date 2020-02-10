9c1e874623ab3fdefa9b4e439239e30c6100f8c7
Evgenia Petukhova <chet444@mail.ru>
2020-02-10 20:00:31 +0500

    change the class name for the slider

--- script.js
+++ script.js
@@ -1,42 +1,41 @@
-let newsHeader = [...document.getElementsByClassName('news__item-header')];
-let newsPhoto = [...document.getElementsByClassName('news__item-photo')];
-let newsText = [...document.getElementsByClassName('news__item-text')];
-let circles = [...document.getElementsByClassName('nav__circle-item')];
-
-const wrapHeader = document.getElementById('nav__news-header');
-const wrapPhoto = document.getElementById('nav__news-photo');
-const wrapText = document.getElementById('nav__news-text');
+const newsHeader = [...document.getElementsByClassName('news__item-header')];
+const newsPhoto = [...document.getElementsByClassName('news__item-photo')];
+const newsText = [...document.getElementsByClassName('news__item-text')];
+const circles = [...document.getElementsByClassName('slider__circle-item')];
+const wrapHeader = document.getElementById('slider__news-header');
+const wrapPhoto = document.getElementById('slider__news-img');
+const wrapText = document.getElementById('slider__news-text');
 const arrow = document.getElementById('arrow');
 
 let i = 0;
 wrapPhoto.setAttribute('src', newsPhoto[i].getAttribute('src'));
 wrapHeader.innerHTML = newsHeader[i].innerHTML;
 wrapText.innerHTML = newsText[i].innerHTML;
-circles[i].classList.add('nav__circle-item-active');
+circles[i].classList.add('slider__circle-item-active');
 
 
 let newsInterval = setInterval(nextNews, 3000);
 
 
 function nextNews() {
-	circles.forEach(function(item) {item.classList.remove("nav__circle-item-active");});
+	circles.forEach(function(item) {item.classList.remove('slider__circle-item-active');});
 	i=(i+1)%6;
 	wrapPhoto.setAttribute('src', newsPhoto[i].getAttribute('src'));
  	wrapHeader.innerHTML = newsHeader[i].innerHTML;
  	wrapText.innerHTML = newsText[i].innerHTML;
- 	circles[i].classList.add('nav__circle-item-active');
+ 	circles[i].classList.add('slider__circle-item-active');
 }
 
 
 
 circles.forEach(function(item) {
 	item.addEventListener('click', event => {
-		circles.forEach(function(elem) {elem.classList.remove("nav__circle-item-active");});
+		circles.forEach(function(elem) {elem.classList.remove('slider__circle-item-active');});
 		i = circles.indexOf(item);
 		wrapPhoto.setAttribute('src', newsPhoto[i].getAttribute('src'));
  		wrapHeader.innerHTML = newsHeader[i].innerHTML;
  		wrapText.innerHTML = newsText[i].innerHTML;
- 	 	circles[i].classList.add('nav__circle-item-active');
+ 	 	circles[i].classList.add('slider__circle-item-active');
 	});
 });
 
@@ -51,6 +50,3 @@ window.addEventListener('scroll', function() {
 		arrow.classList.remove('arrow-active');
 	}
 });
-
-
-
